2901b85f7aa34de253649ecdbce3b55928ee4b42
anthony <chronosconsult66@gmail.com>
2025-06-20 17:54:11 +0200

    fix: ajout des credentials Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jpemckbycegeloepluwe.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZW1ja2J5Y2VnZWxvZXBsdXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzI2NjksImV4cCI6MjA2NTkwODY2OX0.Jx_hQ5aQa3ru8o3S7OQeCULjjbgtclN2AdE5QdJ437c'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
