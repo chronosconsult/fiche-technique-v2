54dd5291c01f0f0cfe88757244c5d8bd924a2af8
chronosconsult <chronosconsult66@gmail.com>
2025-06-20 19:11:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Connexion from "./Connexion";
+import Inscription from "./Inscription";
+import ListeFiches from "./ListeFiches";
+import RequireAuth from "./RequireAuth";
 
 export default function App() {
-  return <div>Application Fiche Technique</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <ListeFiches />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
