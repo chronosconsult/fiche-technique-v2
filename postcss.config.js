007b1e5af55b8c1aa565ab679c6a87b771a66fae
anthony <chronosconsult66@gmail.com>
2025-06-21 15:22:08 +0200

    Fix Tailwind usage in PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const postcssNesting = require('postcss-nesting');
-
 module.exports = {
-  plugins: [
-    postcssNesting(),
-    tailwindcss(),
-    autoprefixer()
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
