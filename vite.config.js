65f7216cf49436ae73f8d9d61069813a88d85876
anthony <chronosconsult66@gmail.com>
2025-07-01 19:00:28 +0200

    Fix alias @ and finalize Vite config for Stripe

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: ["@stripe/stripe-js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   build: {
     rollupOptions: {
-      external: ["@stripe/stripe-js"]
-    }
-  }
+      external: ["@stripe/stripe-js"],
+    },
+  },
 });
